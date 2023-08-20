9640e0f5bf70b05ac35706b0d83196329c813cc5
Paweł Nowakowski <nowacowski@yahoo.com>
2023-08-20 17:07:55 +0200

    Create fortune.sh
    8 ball answer script

---
+++ fortune.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Program to tell a persons fortune
+
+echo -e "\n~~ Fortune Teller ~~\n"
+
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+N=$(( RANDOM % 6 ))
+
+echo Ask a yes or no question:
+
+GET_FORTUNE() {
+  read QUESTION
+  if [[ $QUESTION =~ \?$ ]]
+  then echo -e "\n${RESPONSES[$N]}"
+  else echo Try again. Make sure it ends with a question mark:
+  fi
+}
+
+GET_FORTUNE
+
+until [[ $QUESTION =~ \?$ ]]
+do
+  GET_FORTUNE again
+done
